8459cfb637d5851eec368946bdb0ab01dc891368
Markus <48181660+BackInBash@users.noreply.github.com>
2019-11-22 23:52:51 +0100

    Added Prepare Search Integraion Test

--- DataAnalytics/DeezerSync.Core.IntegrationTest/Prepare.cs
+++ DataAnalytics/DeezerSync.Core.IntegrationTest/Prepare.cs
@@ -2,6 +2,7 @@ using DeezerSync.Models;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
@@ -27,7 +28,7 @@ namespace DeezerSync.Core.IntegrationTest
         }
 
         [Fact]
-        public async Task Integration()
+        public async Task PrepareStrings()
         {
             ILogger<Log.NLogger> logger = new Logger<Log.NLogger>(new NullLoggerFactory());
             DeezerSync.Core.Prepare p = new Core.Prepare(new DeezerSync.Log.NLogger(logger));
@@ -45,6 +46,55 @@ namespace DeezerSync.Core.IntegrationTest
                 Playlist.Add(new StandardPlaylist { description = i.description, id = i.id, provider = i.provider, title = i.title, tracks = Titel });
             }
             await File.WriteAllTextAsync(@"../../../../../DataAnalytics/RawData/PrepareResult.json", JsonConvert.SerializeObject(Playlist, Formatting.Indented));
+        }
+
+        [Fact]
+        public async Task TrackChanges()
+        {
+
+            // Read Values
+            var Playlist = JsonConvert.DeserializeObject<List<StandardPlaylist>>(File.ReadAllText(@"../../../../../DataAnalytics/RawData/PrepareResult.json"));
+
+            int count = 0;
+            int track = 0;
+            int artist = 0;
+            int remixArtist = 0;
+            int isRemix = 0;
+            int unchanged = 0;
+
+            for (int i = 0; i < Playlist.Count; i++)
+            {
+                for (int a = 0; a < Playlist[i].tracks.Count; a++)
+                {
+                    bool changed = false;
+                    if (!Playlist[i].tracks[a].title.Equals(SoundCloud[i].tracks[a].title))
+                    {
+                        track++;
+                        changed = true;
+                    }
+                    if ((Playlist[i].tracks[a].artist ?? string.Empty) != (SoundCloud[i].tracks[a].artist ?? string.Empty))
+                    {
+                        artist++;
+                        changed = true;
+                    }
+                    if ((Playlist[i].tracks[a].remixArtist ?? string.Empty) != (SoundCloud[i].tracks[a].remixArtist ?? string.Empty))
+                    {
+                        remixArtist++;
+                        changed = true;
+                    }
+                    if (!Playlist[i].tracks[a].isRemix.Equals(SoundCloud[i].tracks[a].isRemix))
+                    {
+                        isRemix++;
+                        changed = true;
+                    }
+                    if (!changed)
+                    {
+                        unchanged++;
+                    }
+                    count++;
+                }
+            }
+            await File.AppendAllTextAsync(@"../../../../../DataAnalytics/PrepareSearch.csv", DateTime.Now.ToString() + ", " + count + ", " + track + ", " + artist + ", " + remixArtist + ", " + isRemix + ", " + unchanged + "\n");
         }
     }
 }
